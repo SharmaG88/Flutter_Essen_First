d300fa7762bac7133208a255fcfbded2fc5a836b
fang yuan <gsharma115ks@gmail.com>
2026-06-20 20:16:07 +0530

    new

--- lib/icon.dart
+++ lib/icon.dart
@@ -32,11 +32,53 @@ class Img extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      'https://www.switzerlandical.com/wp-content/uploads/2022/10/Oeschinensee-Calm-Atmosphere.webp',
-      height: 300,
-      width: double.infinity,
-      fit: BoxFit.cover,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Image.network(
+        'https://www.switzerlandical.com/wp-content/uploads/2022/10/Oeschinensee-Calm-Atmosphere.webp',
+        height: 300,
+        width: double.infinity,
+        fit: BoxFit.cover,
+      ),
+      Positioned.fill(
+        child: Center(
+          child: Text(
+            'Oeschinen Lake',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+      Positioned(
+        right: 10,
+        bottom: -20,
+        left:10,
+        child: Center(
+          child: IconButton(
+            onPressed: (){}, 
+            icon: Icon(
+               Icons.favorite,
+               size: 50,
+               color: Colors.amber,
+              ),
+            ),
+          ),
+        ),
+        Center( // ✅ Sab children center mein
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.deepOrange,
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage('https://static.wikia.nocookie.net/reverend-insanity/images/7/7c/Ren_zu_-_novel.jpg/revision/latest?cb=20221216221128'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
